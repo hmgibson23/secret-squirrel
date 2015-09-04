7ec619c647b5c20ef5e2d6d98003d288e123eeb8
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-09-04 10:21:32 +0100

    Remove big files and stuff Add all the files
    Remove big files
    
    Remove big files

---
+++ fetch-tags.py
@@ -0,0 +1,2 @@
+import urllib.request
+urllib.request.urlopen("http://content.guardianapis.com/tags?q=news%2F&api-key=teleporter&page=1").read()
---
+++ trunk/word-2-vec-service.py
@@ -0,0 +1,38 @@
+import gensim, logging, sys
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+MODEL = None
+
+## Logger magic
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+@app.route('/check', methods=['POST'])
+def check():
+    vector = checkProximity(request.json["word"])
+    return jsonify(vector)
+
+
+def loadModel(filename):
+    return gensim.models.Word2Vec.load_word2vec_format(filename, binary=True)
+
+def checkProximity(phrase):
+    return MODEL.most_similar(positive=phrase.split())
+
+
+def setup(argv):
+    if len(argv) != 2:
+        print("./word-2-vec-service <training-set>")
+        sys.exit(1)
+
+        global MODEL
+    MODEL = loadModel(argv[1])
+
+
+
+
+if __name__ == "__main__":
+    setup(sys.argv)
+    app.run()
